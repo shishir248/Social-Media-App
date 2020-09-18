ee536864542683e1340c2900a989e689d1adf604
shishir248 <shishirmanipal40@gmail.com>
2020-09-19 00:02:33 +0530

    Setting up view engine

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const port = 8000;
 //Fire express
 const app = express();
 //Get express router from routes
-app.use('/', require('./routes/'));
-
+app.use("/", require("./routes/"));
+//Setting up view engine
+app.set("view-engine", "ejs");
+app.set("views", "./views");
 
 //To check if server is running
 app.listen(port, function (err) {
-    if (err) {
-        console.log(`Error in running the server : ${err}`);
-        return;
-    }
-    console.log(`Server is running on port: ${port}`);
+  if (err) {
+    console.log(`Error in running the server : ${err}`);
+    return;
+  }
+  console.log(`Server is running on port: ${port}`);
 });
